f0b673a73fd4f17a1347bc1f2003759652ed66a4
NOT-ABOT <notabot1010@gmail.com>
2016-08-15 00:27:33 -0500

    Update vidsbot.py

--- vidsbot.py
+++ vidsbot.py
@@ -1,21 +1,13 @@
 import praw, time, sqlite3, re
 
-
-#######################################################################################
-#This needs to be filled in on your own and should never be shown to anyone           #
-#######################################################################################
-
-
-
 def login():
+    print('Logging in to Reddit as /u/UnknownVideosMod...')
     r = praw.Reddit(user_agent)
     r.set_oauth_app_info(app_id, app_secret, app_uri)
     r.refresh_access_information(refresh_token)
     return r
-    
 r = login()
 r
-
 
 disclaimer = '''\n\n-----------------------------------------------------------------------------------------------------------\n\n
 *I am a bot, and this was done automatically. If you have any questions or concerns regarding the actions of this bot, or feel that this was done in error,
@@ -26,12 +18,8 @@ sub = 'unknownvideos_test'
 username = 'UnknownVideosMod'
 
 usernames = [
-             'unknownvideosmod',
-             'jontheboss']
+             'unknownvideosmod',]
 
-mod = 'jontheboss'
-title ='Found a overposter'
-maxposts = 100
 urls = ['https://www.youtube.com',
         'https://www/youtu.be.com',
        'https://imgur.com',]
@@ -42,13 +30,8 @@ cur = database.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS answered(id TEXT)')
 database.commit()
 
-
-def save():
-    cur.execute('INSERT INTO answered VALUES(?)', [submission.id])
-    database.commit()
-
-def get_submissions():
-    submissions = r.get_subreddit(sub).get_new(limit=maxposts)
+def get_submissions(sub='unknownvideos_test', posts=100):
+    submissions = r.get_subreddit(sub).get_new(limit=posts)
     for submission in submissions:
         if submission.author.name == None:
             continue
@@ -56,31 +39,24 @@ def get_submissions():
         if not cur.fetchone():
             author = submission.author.name.lower()
 
-def remove_post():
-    r.send_message(mod, title, '/u/'+author+' may be breaking the rules. I have deleted the submission, but you should still check it out.')
-    r.send_message(author, 'I\'m sorry', 'I disagree with what those evil humans made me do!')
-    submission.add_comment(removal_message + disclaimer)
-    submission.remove()
+def send(mod='jontheboss', title='Potential Rulebreaker', message='/u/'+author+' may be breaking the rules. [You should check it out]('+str(submission.url)+')'):
+    r.send_message(mod, title, message)
 
-def search_user():
-    user_submissions = r.get_redditor(author).get_submitted(limit=10)
-    for user_submission in user_submissions:
-        if len(re.findall(urls[i], user_submission.url)) > 1:
-            remove_post()
-            
-def check_for_mods():
-    for n in range(len(usernames)):
-        return False if not re.match(usernames[n], author)
-            
-            
+
 def search_posts():
     get_submissions()
-    check_for_mods()
-    if check_for_mods == False:
-            for i in range(len(urls)):
-                if re.match(urls[i], submission.url):
-                    save()
-                    search_user()
+    if not re.match(usernames[n], author) and not any(urls in submission.url):
+        #cur.execute('INSERT INTO answered VALUES(?)', [submission.id])
+        #database.commit()
+        user_submissions = r.get_redditor(author).get_submitted(limit=10)
+        for user_submission in user_submissions:
+            if len(re.findall(urls[i], user_submission.url))  > 1:
+                send()
+                print('Removing potential rulebreaker')
+                submission.add_comment(removal_message + disclaimer)
+                submission.remove()
+
+
 
 load = 1
 while True:
